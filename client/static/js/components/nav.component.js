aa411f778de79fbe71f7c5eef429324cd2a68f9f
Maximilian Kern <maxkernw@gmail.com>
2020-11-06 15:55:07 +0100

    font size

--- client/static/js/components/nav.component.js
+++ client/static/js/components/nav.component.js
@@ -121,7 +121,7 @@ export class NavComponent extends HTMLElement {
           .sidenav a {
             padding: 8px 8px 8px 32px;
             text-decoration: none;
-            font-size: 25px;
+            font-size: 15px;
             color: #818181;
             display: block;
             transition: 0.3s;
